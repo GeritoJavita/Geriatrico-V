7276fc71569abc89497521d10b066595d26a52c4
GeritoJavita <geronimo.garciaalarcon@gmail.com>
2025-10-07 20:36:03 -0500

    pequeños detalles agregados

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,29 +13,39 @@
         </div>
 
         <div class="login-container">
-            <h2 class="login-title">Bienvenido al <span>Hogar Geriátrico</span></h2>
-            <p class="login-subtitle">Por favor, inicia sesión para continuar</p>
+            <div class="login-content">
+                <h2 class="login-title">Bienvenido al <span>Hogar Geriátrico</span></h2>
+                <p class="login-subtitle">Por favor, inicia sesión para continuar</p>
 
-            @if ($errors->any())
-                <div class="error-message">
-                    {{ $errors->first() }}
-                </div>
-            @endif
+                @if ($errors->any())
+                    <div class="error-message">
+                        {{ $errors->first() }}
+                    </div>
+                @endif
 
-            <form method="POST" action="{{ route('login.post') }}" class="login-form">
-                @csrf
-                <div class="form-group">
-                    <label for="email">Correo electrónico</label>
-                    <input type="email" name="email" id="email" required autofocus value="{{ old('email') }}">
-                </div>
+                <form method="POST" action="{{ route('login.post') }}" class="login-form">
+                    @csrf
+                    <div class="form-group">
+                        <label for="email">Correo electrónico</label>
+                        <input type="email" name="email" id="email" required autofocus value="{{ old('email') }}">
+                    </div>
 
-                <div class="form-group">
-                    <label for="password">Contraseña</label>
-                    <input type="password" name="password" id="password" required>
-                </div>
+                    <div class="form-group">
+                        <label for="password">Contraseña</label>
+                        <input type="password" name="password" id="password" required>
+                    </div>
 
-                <button type="submit" class="btn-login-form">Ingresar</button>
-            </form>
+                    <div class="forgot-password">
+                        <a href="Aqui va la ruta al pasword request para cambiarlaxd">¿Olvidaste tu contraseña?</a>
+                    </div>
+
+                    <button type="submit" class="btn-login-form">Ingresar</button>
+                </form>
+            </div>
+
+            <footer class="login-footer">
+                <p>&copy; {{ date('Y') }} Hogar Geriátrico. Todos los derechos reservados. Angeles-V</p>
+            </footer>
         </div>
     </div>
 </body>
